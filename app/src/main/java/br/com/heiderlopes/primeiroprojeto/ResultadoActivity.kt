f92873eb62e0f273adb56fa05e9d7d0fefdc6221
albertlopes <albert.abt@gmail.com>
2018-09-01 12:36:19 -0300

    Atualização da tela

--- app/src/main/java/br/com/heiderlopes/primeiroprojeto/ResultadoActivity.kt
+++ app/src/main/java/br/com/heiderlopes/primeiroprojeto/ResultadoActivity.kt
@@ -14,6 +14,7 @@ class ResultadoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val peso = intent.extras.getString(ConstantesExtra.KEY_PESO)
         val altura = intent.extras.getString(ConstantesExtra.KEY_ALTURA)
@@ -25,6 +26,10 @@ class ResultadoActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT).show()
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
     private fun caucularIMC(peso: Double, altura: Double) {
         val imc = peso / (altura * altura)
 
@@ -35,27 +40,33 @@ class ResultadoActivity : AppCompatActivity() {
             in 0..17 -> {
                 tvIMC.text = imc.format(0)
                 setImagem(R.drawable.magreza)
+                tvIMCDescricao.text = getString(R.string.label_magreza)
             }
             in 17.1..18.5 -> {
                 tvIMC.text = imc.format(0)
                 setImagem(R.drawable.abaixo)
+                tvIMCDescricao.text = getString(R.string.label_magro)
             }
             in 18.6..24.9 -> {
                 tvIMC.text = imc.format(0)
                 setImagem(R.drawable.ideal)
+                tvIMCDescricao.text = getString(R.string.label_ideal)
             }
             in 25.0..29.9 -> {
                 tvIMC.text = imc.format(0)
                 setImagem(R.drawable.sobre)
+                tvIMCDescricao.text = getString(R.string.label_sobre)
             }
             in 30.0..34.9 -> {
                 tvIMC.text = imc.format(0)
                 setImagem(R.drawable.obesidade)
+                tvIMCDescricao.text = getString(R.string.label_gordo)
             }
 
             else -> {
                 tvIMC.setText(imc.format(0))
                 setImagem(R.drawable.obesidademorbida)
+                tvIMCDescricao.text = getString(R.string.label_obesidade)
             }
 
 
